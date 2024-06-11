4684ab1f06c66dfee69b783393a98ddf5bef908f
Mary-Njiru <marynwanjiruu@gmail.com>
2024-06-11 19:53:37 +0300

    Adds file

--- app/src/main/java/com/example/hello/FifthBirdActivity.kt
+++ app/src/main/java/com/example/hello/FifthBirdActivity.kt
@@ -27,6 +27,7 @@ class FifthBirdActivity : AppCompatActivity() {
             .get()
             .load("https://images.unsplash.com/photo-1613891188927-14c2774fb8d7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGJpcmR8ZW58MHx8MHx8fDA%3D")
             .fit()
+
             .into(binding.imageView11)
     }
 }
